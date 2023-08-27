bdae0e067db331900c345f91e0d3ae89df30be2f
charaf2023 <charafeddine.toumi@inttic.dz>
2023-08-27 15:38:38 +0100

    creating docker class

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -2,7 +2,6 @@
 #!/usr/bin/env groovy
 import com.example.Docker
 
-def call(String credentialsId,String repo) {
-    if (!repo) repo=""
+def call(String credentialsId,String repo="") {
     return new Docker(this).dockerLogin(credentialsId,repo)
 }
