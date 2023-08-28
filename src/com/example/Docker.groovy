b2002ed5ca5ac607e794664ff87adf44f0d92b59
charaf2023 <charafeddine.toumi@inttic.dz>
2023-08-28 23:11:16 +0100

    fixed

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -35,8 +35,7 @@ class Docker implements Serializable {
         script.sh "git remote set-url origin https://${script.USER}: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@github.com/${script.USER}/${repo}.git"
         script.sh "git add ."
         script.sh 'git commit -m "ci:version bump"'
-        script.sh 'git checkout jenkins-shared-lib'
-        script.sh "echo 'Charaf.0664827797' | git push "
+        script.sh "echo '${script.PASS}' | git push "
         }
     }
 }
