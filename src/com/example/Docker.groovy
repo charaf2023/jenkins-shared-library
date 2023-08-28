86546b7553a2cf30e483ca71190e1859a24b0f56
charaf2023 <charafeddine.toumi@inttic.dz>
2023-08-29 00:28:19 +0100

    fixed

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -27,11 +27,11 @@ class Docker implements Serializable {
     def pushToGithub(String repo) {
         script.echo "stage of pushing a commit to github in $script.BRANCH_NAME"
         script.withCredentials([script.usernamePassword(credentialsId: 'github-credentials',passwordVariable:'PASS',usernameVariable:'USER')]){
-        script.sh "git config --global user.name \"jenkins\""
-        script.sh "git config --global user.email \"chatou992@gmail.com\""
+        script.sh "git config --global user.name \"charaf2023\""
+        script.sh "git config --global user.email \"charafeddine.toumi@inttic.dz\""
 //        script.sh "git remote set-url origin https://${script.USER}:/${script.USER}/${repo}.git"
 
-        script.sh "curl -u \"charaf2023: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\" https://github.com/charaf2023/devops.git\n"
+        script.sh "curl -u \"charaf2023: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\" https://github.com/charaf2023/devops.git\n"
         script.sh "git add ."
         script.sh "git commit -m 'ci:version bump'"
         script.sh "git push -u origin jenkins-shared-lib"
